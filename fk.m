cfad136a543eff0e288af4eb05219587c37da905
Dan <dstock@seas.upenn.edu>
2017-11-13 18:41:13 -0500

    added ik, simulate. ik and fk successfully tested with simulate

--- fk.m
+++ fk.m
@@ -1,5 +1,8 @@
 %% forward kinematics of exoskeleton
-% angle flexion is positive
+% angle flexion is considered positive
+% zero pose is thigh, shank perpendicular to ground, foot parallel to
+% ground. 
+%
 % inputs - 
 % q = 3x1 joint angle input
 % L = 3xl link lengths (hip-knee, knee-ankle, ankle-foot)
@@ -9,9 +12,9 @@
 
 function X = fk(q, L)
 
-X = zeros(3,1);
-X(1) = L(1)*[sin(q(1)), -cos(q(1))];
+X = zeros(3,2);
+X(1,:) = L(1)*[sin(q(1)), -cos(q(1))];
 theta2 = q(1)-q(2);
-X(2) = X(1) + L(2)*[sin(theta2), -cos(theta2)];
+X(2,:) = X(1,:) + L(2)*[sin(theta2), -cos(theta2)];
 theta3 = theta2 + pi/2 + q(3);
-X(3) = X(2) + L(3)*[sin(theta3), -cos(theta3)];
+X(3,:) = X(2,:) + L(3)*[sin(theta3), -cos(theta3)];
---
+++ ik.m
@@ -0,0 +1,36 @@
+%% inverse kinematics of exoskeleton
+% angle flexion is considered positive
+% zero pose is thigh, shank perpendicular to ground, foot parallel to
+% ground. 
+%
+% inputs - 
+% X = 1x2 ankle position
+% L = 3xl link lengths (hip-knee, knee-ankle, ankle-foot)
+% outputs - 
+% q = 3x1 joint angle input
+% note: q3 output is joint angle that makes foot parallel to ground
+% note: for low magnitude q2 (+- 10 degrees), both flexion and extension
+% are valid answers. The flexion joint angle is always returned.
+
+function [q, possible] = ik(X, L)
+
+%   % check validitiy
+L1 = L(1);
+L2 = L(2);
+% hip to ankle distance
+L3 = sqrt(X(1)^2+X(2)^2);
+
+possible = L3 <= L1+L2;
+
+q = zeros(3,1);
+
+if (possible)
+    %% calculate
+    theta = atan2(X(1), -X(2));
+    % acos ensures phi > 0, meaning knee always in flexion
+    phi = acos((L1^2+L3^2-L2^2)/(2*L1*L3));
+
+    q(1) = theta+phi;
+    q(2) = pi - acos((L1^2+L2^2-L3^2)/(2*L1*L2));
+    q(3) = q(2)-q(1);
+end
