0642fb0fd6aea842d2687c0a46258df62ff40e27
Dan <dstock@seas.upenn.edu>
2017-11-13 14:05:56 -0500

    fk first draft

---
+++ fk.m
@@ -0,0 +1,17 @@
+%% forward kinematics of exoskeleton
+% angle flexion is positive
+% inputs - 
+% q = 3x1 joint angle input
+% L = 3xl link lengths (hip-knee, knee-ankle, ankle-foot)
+% outputs - 
+% X = 3x2 knee, ankle, and end of foot positions
+
+
+function X = fk(q, L)
+
+X = zeros(3,1);
+X(1) = L(1)*[sin(q(1)), -cos(q(1))];
+theta2 = q(1)-q(2);
+X(2) = X(1) + L(2)*[sin(theta2), -cos(theta2)];
+theta3 = theta2 + pi/2 + q(3);
+X(3) = X(2) + L(3)*[sin(theta3), -cos(theta3)];
