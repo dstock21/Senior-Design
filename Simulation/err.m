eaba9a4d1febbf7ab19f9eb4fdd58bbf5d33f4f3
Tonto95 <owusua@seas.upenn.edu>
2018-04-07 16:07:33 -0400

    Add files via upload

---
+++ Simulation/err.m
@@ -0,0 +1,19 @@
+function e = err(q, qr)
+
+err = inf;
+for k = 1:length(qr)
+    temp = (q(1)-qr(k,1))^2 + (q(2) - qr(k,2))^2;
+    if temp < err
+        err = temp;
+        ir = k;
+    end
+end
+
+err = abs(q-qr(ir,:))-3;
+
+for i = 1:length(err)
+    if err(i) < 0
+        err(i) = 0;
+    end
+end
+e = 0.1*err.^2;
